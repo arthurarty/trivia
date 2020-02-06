f30a916cbaa5bf11cae5635aa27fbe2ca53704e8
arthurarty <arthur.nangai@gmail.com>
2020-02-06 23:30:46 +0300

    ft(questions) - Implement create question.
    - Enforce required fields.
    - Add tests.
    - Specifiy version of PSQL for travis.

---
+++ backend/sample_data.py
@@ -0,0 +1,19 @@
+new_question = {
+	"question": "Who is Jim married to?",
+	"answer": "Jim Herbert",
+	"category": 2,
+	"difficulty": 2
+}
+
+incomplete_question = {
+    "question": "Who is Jim married to?",
+	"category": 2,
+	"difficulty": 2
+}
+
+unprocessable_question = {
+    "question": "Who is Jim married to?",
+	"answer": "Jim Herbert",
+	"category": "string",
+	"difficulty": 2
+}
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+from sample_data import new_question, incomplete_question, unprocessable_question
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -17,8 +18,9 @@ class TriviaTestCase(unittest.TestCase):
         self.database_name = "trivia_test"
         self.database_password = "qwe"
         self.database_path = f"postgresql://postgres:{self.database_password}@localhost:5432/{self.database_name}"
-        setup_db(self.app, self.database_path)
 
+        setup_db(self.app, self.database_path)
+        
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
@@ -49,9 +51,7 @@ class TriviaTestCase(unittest.TestCase):
     def test_delete_question(self):
         res = self.client().delete('/questions/9')
         data = json.loads(res.data)
-
         question = Question.query.filter(Question.id == 9).one_or_none()
-
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertEqual(data['deleted'], 9)
@@ -61,10 +61,34 @@ class TriviaTestCase(unittest.TestCase):
     def test_404_if_question_doesnt_exist(self):
         res = self.client().delete('/questions/1000')
         data = json.loads(res.data)
-
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'Resource not found')
+
+    def test_create_new_question(self):
+        res = self.client().post('/questions', json=new_question)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(data['success'], True)
+
+    def test_create_incomplete_question(self):
+        """
+        question missing required field should
+        result in 400.
+        """
+        res = self.client().post('/questions', json=incomplete_question)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+
+    def test_unprocessable_question(self):
+        """
+        Unprocessable question
+        """
+        res = self.client().post('/questions', json=unprocessable_question)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
